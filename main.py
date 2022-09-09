a0ff12dcc6bc5000ed60363a9113aad583c18d1b
louis-br <46930119+louis-br@users.noreply.github.com>
2022-09-09 00:59:20 +0000

    Add naive average filter

--- main.py
+++ main.py
@@ -6,13 +6,26 @@ import cv2
 INPUT_IMAGE_PREFIX =  'a'
 INPUT_IMAGE = '01 - Original.bmp'
 
+def filtro_media_ingenuo(img, janela=3):
+    meia_janela = janela//2
+    janela_total = (janela)**2
+    altura, largura, canais = img.shape
+    for y in range(meia_janela, altura - meia_janela):
+        for x in range(meia_janela, largura - meia_janela):
+            for c in range(canais):
+                soma = 0.0
+                for dy in range(-meia_janela, meia_janela + 1):
+                    for dx in range(-meia_janela, meia_janela + 1):
+                        soma += img[y + dy, x + dx, c]
+                img[y, x, c] = soma/janela_total
+    pass
 
 def main ():
-    INPUT_IMAGE_PREFIX = sys.argv[1] if len(sys.argv) > 1 else INPUT_IMAGE_PREFIX
-    INPUT_IMAGE = INPUT_IMAGE_PREFIX + INPUT_IMAGE
+    IMAGE_PREFIX = sys.argv[1] if len(sys.argv) > 1 else INPUT_IMAGE_PREFIX
+    INPUT = IMAGE_PREFIX + INPUT_IMAGE
 
     # Abre a imagem em 3 canais BGR.
-    img = cv2.imread (INPUT_IMAGE, cv2.IMREAD_COLOR)
+    img = cv2.imread (f'exemplos/{INPUT}', cv2.IMREAD_COLOR)
     if img is None:
         print ('Erro abrindo a imagem.\n')
         sys.exit ()
@@ -20,10 +33,9 @@ def main ():
     # Convertemos para float32.
     img = img.astype (np.float32) / 255
 
-    # Mantém uma cópia colorida para desenhar a saída.
-    #img_out = cv2.cvtColor (img, cv2.COLOR_GRAY2BGR)
+    filtro_media_ingenuo(img, 3)
 
-    #cv2.imwrite ('01 - out.png', img*255)
+    cv2.imwrite (f'out/{IMAGE_PREFIX}01 - Borrada.png', img*255)
 
     #start_time = timeit.default_timer ()
     
